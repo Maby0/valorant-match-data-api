fca6ab3f7b56bccf76015470e735931582b27e6c
mabon <mabonapgwyn@hotmail.co.uk>
2024-03-20 15:45:31 +0000

    add lloyd smurf name

--- src/constants/player-names.ts
+++ src/constants/player-names.ts
@@ -1,6 +1,8 @@
 export const GARRIN_NAME = 'Jetpack'
+export const GARRIN_SMURF_NAME = 'VELOCITYHEAVENS'
 export const JAC_NAME = 'ItsJustMono'
 export const LLOYD_NAME = 'Cornishiwa'
+export const LLOYD_SMURF_NAME = 'BitterSun'
 export const LLYWELYN_NAME = 'Llywelyn'
 export const MABON_NAME = 'Mabyo'
 export const MABON_SMURF_NAME = 'seb0n'
@@ -9,8 +11,10 @@ export const SION_NAME = 'GingerBinger'
 export const SOPHIE_NAME = 'Scl'
 
 export const GARRIN_TAG = '6350'
+export const GARRIN_SMURF_TAG = '3606'
 export const JAC_TAG = 'EUW'
 export const LLOYD_TAG = 'EUW'
+export const LLOYD_SMURF_TAG = 'EUW'
 export const LLYWELYN_TAG = 'EUW'
 export const MABON_TAG = 'EUW'
 export const MABON_SMURF_TAG = 'EUW'
@@ -20,20 +24,24 @@ export const SOPHIE_TAG = '7277'
 
 export const TEAM_INFO = [
   { name: GARRIN_NAME, tag: GARRIN_TAG },
+  { name: GARRIN_SMURF_NAME, tag: GARRIN_SMURF_TAG },
   { name: JAC_NAME, tag: JAC_TAG },
   { name: LLOYD_NAME, tag: LLOYD_TAG },
+  { name: LLOYD_SMURF_NAME, tag: LLOYD_SMURF_TAG },
   { name: LLYWELYN_NAME, tag: LLYWELYN_TAG },
   { name: MABON_NAME, tag: MABON_TAG },
   { name: MABON_SMURF_NAME, tag: MABON_SMURF_TAG },
   { name: OWAIN_NAME, tag: OWAIN_TAG },
-  { name: SOPHIE_NAME, tag: SOPHIE_TAG },
-  { name: SION_NAME, tag: SION_TAG }
+  { name: SION_NAME, tag: SION_TAG },
+  { name: SOPHIE_NAME, tag: SOPHIE_TAG }
 ]
 
 export const TEAM_NAMES_LIST = [
   GARRIN_NAME,
+  GARRIN_SMURF_NAME,
   JAC_NAME,
   LLOYD_NAME,
+  LLOYD_SMURF_NAME,
   LLYWELYN_NAME,
   MABON_NAME,
   MABON_SMURF_NAME,
